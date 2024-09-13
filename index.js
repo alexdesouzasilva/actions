63858690a0260ce3ec2fdaa67b398b42b5462673
Alex <alexdesouzasilva94@gmail.com>
2024-09-13 11:33:46 -0300

    interpolation

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const github = require("@actions/github")
 console.log("Iniciando processo...")
 try {
     const name = core.getInput('name');
-    console.log(`Hello ${name}!`);
     const message = `Welcome mister ${name}`
     core.setOutput("message", message);
 
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    console.log('The event payload: ${payload}');
+    console.log(`The event payload: ${payload}`);
 } catch(error) {
     core.setFailed(error.message);
 }
